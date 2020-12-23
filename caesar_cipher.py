23ccceec34677ed40755237e73896f70c8f41b19
Gerard Gandionco <70365928+Vaiterius@users.noreply.github.com>
2020-12-23 03:25:55 -0800

    Add files via upload

---
+++ caesar_cipher.py
@@ -0,0 +1,52 @@
+"""
+Caesar Cipher
+by Wesley Jiang and Gerard Gandionco
+12/22/2020
+"""
+
+def cipher(key: int, message: str) -> str:
+    """
+    Returns a coded message
+    - also able to decode messages with negative key 
+    """
+
+    alpha_list = "abcdefghijklmnopqrstuvwxyz"
+    alpha_list2 = alpha_list
+
+    new_list = alpha_list2[key%26:] + alpha_list2[:key%26]  # Shifts cipher key amount
+
+    new_message = []
+    
+    for char in message:
+        if not char.isalpha():  # If the char is a anything but an alphabetical letter
+            new_message.append(char)
+        for i in range(len(alpha_list)):  # Matches cipher to corresponding letter
+            if alpha_list[i] == char:
+                new_message.append(new_list[i])
+
+    new_new_message = "".join(new_message)  # Turns list into single string
+    return new_new_message
+
+if __name__ == "__main__":
+
+    # Takes user input for key (an int) and a message (a str)
+    while True:
+        try:
+            print("Enter 'quit' to exit out anytime")
+            print("\nEnter your key")
+            key1 = input("> ")
+            if key1 == "quit":
+                break
+            key2 = int(key1)
+
+            print("Enter your message")
+            plaintext = input("> ")
+            if plaintext == 'quit':
+                break
+
+        except ValueError:
+            print("Please enter an integer\n")
+        else:
+            # Calls cipher function and returns new encrypted message
+            new_message = cipher(key2, plaintext)
+            print(f"Your new message is: {new_message}\n")
